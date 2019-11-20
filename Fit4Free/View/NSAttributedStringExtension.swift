4d3b3307ce55b566e831463b3a9345661338e5f0
Angel Figueroa Rivera <angelfigueroarivera@Angels-MacBook-Pro.local>
2019-11-19 20:08:48 -0600

    hyperlink y constraints en ultima vista

---
+++ Fit4Free/View/NSAttributedStringExtension.swift
@@ -0,0 +1,9 @@
+//
+//  NSAttributedStringExtension.swift
+//  Fit4Free
+//
+//  Created by Angel Figueroa Rivera on 11/19/19.
+//  Copyright © 2019 Jaime Garza. All rights reserved.
+//
+
+import Foundation
